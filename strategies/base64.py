166d63ed83d53a9e5828ac0dd18a92440c6d96f7
waseem-code7 <mohammedwaseem2601@gmail.com>
2025-05-02 20:40:23 +0530

    shortened url base 64 strategy

---
+++ strategies/base64.py
@@ -0,0 +1,8 @@
+import base64
+
+from strategies.abstract_shortener import AbstractShortener
+
+
+class Base64Encoder(AbstractShortener):
+    def get_shortened_url_id(self, data: str) -> str:
+        return base64.b64encode(data.encode('utf-8')).decode("utf-8")
