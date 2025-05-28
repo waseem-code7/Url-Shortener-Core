8b4cffca82faa615514830d94fd75bab012303b7
waseem-code7 <mohammedwaseem2601@gmail.com>
2025-05-28 21:46:44 +0530

    added kafka producer code

--- services/kafka.py
+++ services/kafka.py
@@ -1,10 +1,10 @@
-import asyncio
+import json
 import os
 import threading
 import socket
+import aiokafka
 
-from confluent_kafka import Producer
-from types.custom_dict import KafkaProducerMessage
+from custom_types.custom_dict import KafkaProducerMessage
 from common.utils import get_logger
 logger = get_logger(__name__)
 
@@ -15,11 +15,19 @@ class KafkaProducer:
 
     def __init__(self):
         conf = {
-            'bootstrap.servers': os.getenv("KAFKA_BROKERS"),
-            'client.id': socket.gethostname()
+            'bootstrap_servers': os.getenv("KAFKA_BROKERS"),
+            'client_id': socket.gethostname(),
+            'enable_idempotence': True,
+            'request_timeout_ms': 3000,
+            'retry_backoff_ms': 500,
         }
-        self.producer = Producer(conf)
+        self.producer = aiokafka.AIOKafkaProducer(**conf)
         logger.info("Successfully initialized kafka brokers")
+
+    async def start_producer(self):
+        logger.info("Attempting to start kafka producer")
+        await self.producer.start()
+        logger.info("Successfully started kafka producer")
 
     def __new__(cls, *args, **kwargs):
         if cls._instance is None:
@@ -28,22 +36,15 @@ class KafkaProducer:
                     cls._instance = super().__new__(cls)
         return cls._instance
 
-    def poll_producer(self):
-        self.producer.poll()
+    def stop_producer(self):
+        self.producer.stop()
 
-
-    def send_message(self, topic_name: str, attributes: KafkaProducerMessage, retries=3):
-
-        def acked(err, msg):
-            if err is not None:
-                logger.info("Failed to deliver message: %s: %s" % (str(msg), str(err)))
-
-                if retries > 0:
-                    logger.info("Retrying sending message to kafka ::: ")
-                    return self.send_message(topic_name, attributes, retries - 1)
-                else:
-                    logger.error("All attempts failed to push to kafka hence returning")
-            else:
-                logger.info("Message produced: %s" % (str(msg)))
-
-        self.producer.produce(topic_name, key=attributes.get("key"), value=attributes.get("value"), callback=acked)
+    async def send_message(self, topic_name: str, attributes: KafkaProducerMessage):
+        try:
+            message = json.dumps(attributes.get("value")).encode("utf-8")
+            key = attributes.get("key").encode("utf-8")
+            logger.info(f"Attempting to send message to kafka ::: {message}")
+            await self.producer.send_and_wait(topic=topic_name, value=message, key=key)
+            logger.info("Successfully sent message to kafka")
+        except Exception as e:
+            logger.error("Error occurred while sending message to kafka :::  "+ str(e))
