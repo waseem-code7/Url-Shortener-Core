90e477f670c79169645bd1795ffbc5c8545eaf69
waseem-code7 <mohammedwaseem2601@gmail.com>
2025-06-21 12:11:17 +0530

    updated session store class name

--- sessions/stores/base.py
+++ sessions/stores/base.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 from typing import Dict, Optional, Any
 
 
-class Session(ABC):
+class SessionStore(ABC):
     """Abstract base class for session storage"""
 
     @abstractmethod
