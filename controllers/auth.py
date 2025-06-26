e785fe0d5f325c810a8ed95f0d416e585afeacc5
waseem-code7 <mohammedwaseem2601@gmail.com>
2025-06-26 23:34:01 +0530

    added auth routers

--- controllers/auth.py
+++ controllers/auth.py
@@ -1,26 +1,23 @@
-from typing import Annotated
-
-from fastapi import APIRouter, Body
+from fastapi import APIRouter, Body, HTTPException
+from fastapi.params import Depends
+from starlette import status
 from starlette.requests import Request
 
+from registries.registry import ServiceRegistry
+from services.auth import AuthService
 from sessions.session import Session
 
 router = APIRouter()
 
-@router.post("/login")
-async def login_user(request: Request, body: dict = Body()):
-    session: Session = request.state.session
-    user_name = body.get("username")
-    session["user_name"] = user_name
-    session["user"] = {
-        "name": "Waseem",
-        "type": "MIDDLEWARE_TEST"
-    }
-    return {"Status": "Success"}
+def get_auth_service() -> AuthService:
+    return ServiceRegistry.get_registry("AUTH_SERVICE")
 
-@router.post("/test_cookie")
-async def login_user(request: Request, body: dict = Body()):
-    session: Session = request.state.session
-    print(session)
-    return {"Status": "Success"}
-
+@router.post("v1/login")
+def login(request: Request, body: dict = Body(), auth_service: AuthService = Depends(get_auth_service)):
+    try:
+        session: Session = request.state.session
+        response = auth_service.verify_credentials(body.get("email_id"), body.get("password"))
+        session["user"] = response.get("user")
+        return {"Status": "Success"}
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail=str(e))
